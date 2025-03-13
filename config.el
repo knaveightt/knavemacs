a3e573438118a8d59e9205b079339545a871b1ec
John O <johnosinski80@gmail.com>
2025-03-12 22:18:25 -0400

    significant restructuring for emacs 30.1

--- config.el
+++ config.el
@@ -5,60 +5,78 @@
 ;; Default Settings - these are loaded if the knavemacs/device-profile
 ;; is not recongized as a value that triggers a profile function
 (setq knavemacs/config-visual nil)
-					; config-visual manages [doom-themes]
-					; [whick-key]
-					; [vundo]
-					; [activities]
-
-(setq knavemacs/config-visual-icons nil)
-					; config-visual-icons manages [nerd-icons]
-					; [nerd-icons-dired]
-					; [nerd-icons-completion]
-					; [nerd-icons-ibuffer]
+;; [Colorscheme]
+;; ef-themes
+;; rainbow-delimiters
+;; rainbow-mode
+;; dashboard
 
 (setq knavemacs/config-fonts nil)
-					; config-fonts manages fonts and [ligatures]
+;; [Font Configuration]
+;; (default-frame-alist)
+;; nerd-icons
+;; nerd-icons-dired
+;; nerd-icons-completion
+;; nerd-icons-ibuffer
+;; [Mode Line Configuration]
+;; (mode-line-format) {1/2}
 
-(setq knavemacs/config-keybinds nil)
-					; config-keybinds manages [meow]
-					; [avy]
-					; [ace-window]
-(setq knavemacs/config-fuzzyfind nil)
-					; config-fuzzyfind manages [vertico]
-					; [savehist]
-					; [marginalia]
-					; [orderless]
-					; [embark]
-					; [consult]
-					; [embark-consult]
-
-(setq knavemacs/config-snippets nil)
-					; config-snippets manages [yasnippets]
-
-(setq knavemacs/config-programming nil)
-					; config-programming manages [magit]
+(setq knavemacs/config-utility nil)
+;; [Key Indicators]
+;; which-key
+;; [Fuzzy Finding Completions]
+;; vertico
+;; savehist
+;; marginalia
+;; orderless
+;; embark
+;; consult
+;; embark-consult
+;; vundo
+;; activities
 
 (setq knavemacs/config-dired nil)
-					; config-dired manages dired customizations
-					; [dired-single]
-					; [dired-hide-dotfiles]
-					; [dired-subtree]
+;; [Dired Configuration]
+;; dired-hide-dotfiles
+;; dired-subtree
 
-(setq knavemacs/config-org nil)
-					; config-org manages org mode configurations
+(setq knavemacs/config-productivity nil)
+;; [Org Mode]
+;; org
+;; [Note Taking System]
+;; markdown-mode
+;; howm
 
+(setq knavemacs/config-programming nil)
+;; [Snippets]
+;; yasnippets
+;; [Eglot Language Server]
+;; eglot
+;; [In-Buffer Completions at Point]
+;; corfu
+;; kind-icon
+
+
+(setq knavemacs/config-keybinds nil)
+;; [Mode Line Configuration]
+;; (mode-line-format) {1/2}
+;; [Jump-To-Location]
+;; avy
+;; [Window Navigation]
+;; ace-window
+;; [Modal Editing]
+;; meow
 ;; Profile definitions
+
 (defun knavemacs/profile-computer ()
   (message "[Knavemacs] Loading Profile for *Computer*")
   (setq knavemacs/config-visual t)
-  (setq knavemacs/config-visual-icons t)
   (setq knavemacs/config-fonts t)
-  (setq knavemacs/config-keybinds t)
-  (setq knavemacs/config-fuzzyfind t)
-  (setq knavemacs/config-snippets t)
-  (setq knavemacs/config-programming t)
+  (setq knavemacs/config-utility t)
   (setq knavemacs/config-dired t)
-  (setq knavemacs/config-org t))
+  (setq knavemacs/config-productivity t)
+  (setq knavemacs/config-programming t)
+  (setq knavemacs/config-keybinds t))
 
 ;; Profile Determination Logic
 (if (eq knavemacs/device-profile 'computer)
