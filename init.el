8f8b8abe8f5fb1fd334da6e8be2471cc7376d7c3
John O <johnosinski80@gmail.com>
2025-07-18 10:23:00 -0400

    added minimal tree-sitter config

--- init.el
+++ init.el
@@ -1007,6 +1007,21 @@
   (global-diff-hl-mode))
 
 ;; --------------------------------------------------
+;;; {programming} treesitter
+;; - tree-sitter
+;; - tree-sitter-langs
+(use-package tree-sitter
+  :ensure t
+  :config
+  (global-tree-sitter-mode))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter
+  :config
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+;; --------------------------------------------------
 ;;; {visual} rainbow-mode
 ;; --------------------------------------------------
 (use-package rainbow-mode
