c698ebcbb5f594df10cec40aaa8fa2acce4ef906
John Osinski <johnosinski80@gmail.com>
2025-08-21 23:14:53 -0400

    more tweaks to modal zap command

--- init.el
+++ init.el
@@ -972,8 +972,8 @@
    ("X" backward-delete-char-untabify) ; reverse delete character (backspace)
    ("y" yank) ; yank
    ("Y" yank-pop) ; yank from kill ring (fuzzy select)
-   ("z" zap-up-to-char :exit t) ; zap up to char
-   ("Z" zap-to-char :exit t)) ; zap including char
+   ("z" zap-up-to-char) ; zap up to char
+   ("Z" zap-up-to-char :exit t)) ; zap up to char and go into insert mode
 
   (ryo-modal-keys
    ;; First argument to ryo-modal-keys may be a list of keywords.
