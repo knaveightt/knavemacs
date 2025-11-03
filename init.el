88341fbe97c8d6edea16f87d75657d7dda110674
John Osinski <johnosinski80@gmail.com>
2025-11-02 21:08:13 -0500

    full modeline updates

--- init.el
+++ init.el
@@ -1035,6 +1035,8 @@
 ;; ==================================================
 (defface knavemacs/modeline-faces-modal
   '((t :foreground "#A720CC"
+       :background "#999999"
+       :weight bold
   	   ))
   "Default Face"
   :group 'knavemacs/mode-line-faces)
@@ -1065,15 +1067,48 @@
 
 (defface knavemacs/modeline-faces-git-additions
   '((t :foreground "#CC0"
+       :background "#000000"
   	   ))
   "Default Face"
   :group 'knavemacs/mode-line-faces)
 
 (defface knavemacs/modeline-faces-git-deletions
   '((t :foreground "#CC0000"
+       :background "#000000"
   	   ))
   "Default Face"
   :group 'knavemacs/mode-line-faces)
+
+(defface knavemacs/modeline-faces-header-sep
+  '((t :foreground "#999999"
+       :background "#444444"
+  	   ))
+  "Default Face"
+  :group 'knavemacs/mode-line-faces)
+
+(defface knavemacs/modeline-faces-header-sep2
+  '((t :foreground "#444444"
+       :background "#111111"
+  	   ))
+  "Default Face"
+  :group 'knavemacs/mode-line-faces)
+
+(defface knavemacs/modeline-faces-header-sep3
+  '((t :foreground "#111111"
+       :background "#030303"
+  	   ))
+  "Default Face"
+  :group 'knavemacs/mode-line-faces)
+
+(defface knavemacs/modeline-faces-mid-background
+  '((t :background "#000000"
+       :foreground "#0011A8"
+  	   ))
+  "Default Face"
+  :group 'knavemacs/mode-line-faces) 
+
+(set-face-attribute 'mode-line nil :background "#111111")
+(set-face-attribute 'mode-line-inactive nil :background "#030303")
 
 ;; ------------MODELINE MODULES
 
@@ -1088,7 +1123,7 @@
 (defvar-local knavemacs/modeline-readonly-indicator
     '(:eval
   	  (when buffer-read-only
-        (propertize "" 'face 'knavemacs/modeline-faces-readonly)))
+        (propertize " " 'face 'knavemacs/modeline-faces-readonly)))
   "Modeline module to provide a readonly indicator for appropriate buffers")
 
 ;; modeline module: modified indicator
@@ -1115,7 +1150,7 @@
 (defvar-local knavemacs/modeline-major-mode-name
     '(:eval
       (when (mode-line-window-selected-p)
-        mode-name))
+        (propertize (concat (format-mode-line mode-name) " ") 'face 'knavemacs/modeline-faces-mid-background)))
   "Modeline module to provide major mode name.")
 
 ;; modeline module: right display
@@ -1146,15 +1181,44 @@
         (propertize (knavemacs/return-git-diff-deletions) 'face 'knavemacs/modeline-faces-git-deletions)))
   "Modeline module to provide version control details.")
 
+;; modeline module: header seperator
+(defvar-local knavemacs/modeline-header-sep
+    '(:eval
+      (when (mode-line-window-selected-p)
+	(propertize (knavemacs/modeline-return-sep) 'face 'knavemacs/modeline-faces-header-sep)))
+  "Modeline module to simply provide a char for seperation")
+
+(defvar-local knavemacs/modeline-header-sep2
+    '(:eval
+      (when (mode-line-window-selected-p)
+	(propertize (knavemacs/modeline-return-sep) 'face 'knavemacs/modeline-faces-header-sep2)))
+  "Modeline module to simply provide a char for seperation")
+
+(defvar-local knavemacs/modeline-header-sep3
+    '(:eval
+      (when (mode-line-window-selected-p)
+	(propertize (knavemacs/modeline-return-sep) 'face 'knavemacs/modeline-faces-header-sep3)))
+  "Modeline module to simply provide a char for seperation")
+
+(defvar-local knavemacs/modeline-header-sep4
+    '(:eval
+      (when (mode-line-window-selected-p)
+	(propertize (knavemacs/modeline-return-sep2) 'face 'knavemacs/modeline-faces-header-sep3)))
+  "Modeline module to simply provide a char for seperation")
+
 ;; ------------MODELINE PREPARE VARIABLES
 (dolist (construct '(knavemacs/modeline-modal-indicator
+					 knavemacs/modeline-major-mode-icon
+  					 knavemacs/modeline-bufname
 					 knavemacs/modeline-readonly-indicator
   					 knavemacs/modeline-modified-indicator
-  					 knavemacs/modeline-bufname
-					 knavemacs/modeline-major-mode-icon
 					 knavemacs/modeline-major-mode-name
 					 knavemacs/modeline-vc-details-additions
 					 knavemacs/modeline-vc-details-deletions
+					 knavemacs/modeline-header-sep
+					 knavemacs/modeline-header-sep2
+					 knavemacs/modeline-header-sep3
+					 knavemacs/modeline-header-sep4
   					 knavemacs/modeline-right-display
   					 knavemacs/modeline-kmacro-indicator))
   (put construct 'risky-local-variable t)) ;; required for modeline local vars
@@ -1162,16 +1226,21 @@
 ;; ------------MODELINE FUNCTIONS
 (defun knavemacs/modeline-fill-for-alignment ()
   "Modeline module to provide filler space until right-aligned items are added to modeline."
-  (let ((r-length (length (concat (format-mode-line knavemacs/modeline-right-display) (format-mode-line knavemacs/modeline-kmacro-indicator)) )))
+  (let ((r-length (length (concat
+                           "   "
+                           (format-mode-line knavemacs/modeline-right-display)
+                           (format-mode-line knavemacs/modeline-kmacro-indicator)
+                           (format-mode-line knavemacs/modeline-major-mode-name)))))
     (propertize " "
-                'display `(space :align-to (- right ,r-length)))))
+                'display `(space :align-to (- right ,r-length))
+                'face 'knavemacs/modeline-faces-mid-background)))
 
 (defun knavemacs/return-modal-state ()
   "Returns the current viper state, or a default string if void."
   (interactive)
   (if ryo-modal-mode
-	  (setq modal-mode-string "󰷈 MEdit")
-	(setq modal-mode-string " Emacs"))
+	  (setq modal-mode-string " 󰷈 MEdit ")
+	(setq modal-mode-string "  Emacs "))
   (format-mode-line 'modal-mode-string))
 
 ;; clocks to throttle the additions/deletions functions to run after some time
@@ -1193,6 +1262,8 @@
                     (setq git-modeline-additions (format "  %s" (match-string 1 diff-output)))))))))
         ; we comment this out and expect the deletions function to reset the clock
         ; (setq git-modeline-last-update (float-time))
+        (when (not (buffer-file-name))
+          (setq git-modeline-additions ""))
         ))
   (format-mode-line 'git-modeline-additions))
 
@@ -1207,27 +1278,40 @@
                 (let ((diff-output (vc-git--run-command-string file "diff" "--numstat" "--" file)))
                   (when (and diff-output (string-match "^\\([0-9]+\\)\t\\([0-9]+\\)\t" diff-output))
                     (setq git-modeline-deletions (format "  %s" (match-string 2 diff-output)))))))))
+        (when (not (buffer-file-name))
+          (setq git-modeline-deletions ""))
 	    (setq git-modeline-last-update (float-time))))
   (format-mode-line 'git-modeline-deletions))
+
+(defun knavemacs/modeline-return-sep ()
+  "Return a seperator to be used in the modeline."
+  "")
+
+(defun knavemacs/modeline-return-sep2 ()
+  "Return a seperator to be used in the modeline."
+  "")
 
 ;; ------------MODELINE CONSTRUCTION
 (setq-default mode-line-format
   			  '("%e"
-  				mode-line-front-space
+  				;mode-line-front-space
   				knavemacs/modeline-modal-indicator
+				knavemacs/modeline-header-sep
+				knavemacs/modeline-header-sep2
+                " "
+  				knavemacs/modeline-major-mode-icon
+                " "
+  				knavemacs/modeline-bufname
   				knavemacs/modeline-readonly-indicator
   				" "
   				knavemacs/modeline-modified-indicator
   				" "
-  				knavemacs/modeline-bufname
-  				" "
-  				knavemacs/modeline-major-mode-icon
-  				" "
-  				knavemacs/modeline-major-mode-name
-				" "
+				knavemacs/modeline-header-sep3
 				knavemacs/modeline-vc-details-additions
 				knavemacs/modeline-vc-details-deletions
   				(:eval (knavemacs/modeline-fill-for-alignment))
+  				knavemacs/modeline-major-mode-name
+				knavemacs/modeline-header-sep4
   				knavemacs/modeline-right-display
   				knavemacs/modeline-kmacro-indicator))
 
