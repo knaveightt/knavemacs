c8578944e3d628aed0209c7dc2c6f78204e0d44e
John O <johnosinski80@gmail.com>
2025-12-16 15:58:34 -0500

    Update org-bullets.el to fix indent face colors

--- knavemacs_visual/org-bullets.el
+++ knavemacs_visual/org-bullets.el
@@ -7,4 +7,11 @@
   (add-hook 'org-mode-hook 'org-bullets-mode)
   :custom
   (org-bullets-bullet-list '("◉" "☯" "○" "☯" "✸" "☯" "✿" "☯" "✜" "☯" "◆" "☯" "▶"))
-  (org-ellipsis " ⤵"))
+  (org-ellipsis " ⤵")
+  :config
+  ;; a fix for color issues with org-indent mode
+  (defun knavemacs/org-indent-fix-colors ()
+    "Function to adjust some faces when org-bullets are used with org indent mode"
+    (set-face-attribute 'org-hide nil :background "#181a26" :foreground "#181a26")
+    (set-face-attribute 'org-indent nil :background "#181a26" :foreground "#181a26"))
+  (advice-add 'org-indent-mode :after #'knavemacs/org-indent-fix-colors))
