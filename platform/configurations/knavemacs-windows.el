fd32666f3f0e617b970a92c4bc9b14c093ccbcaa
John O <johnosinski80@gmail.com>
2026-04-28 14:29:25 -0400

    updated windows-specific org capture templates

--- platform/configurations/knavemacs-windows.el
+++ platform/configurations/knavemacs-windows.el
@@ -3,11 +3,12 @@
 
 ;; capture templates
 (setq org-capture-templates
-      '(	
-	  ("t" "Todo" entry (file+olp "C:\\Users\\josinski\\OneDrive - JNJ\\org\\todos.org" "TODOs")
-         "* %^{Enter Context} %^G\n** %^{Task Type|TODO|PROJECT} %?\n" :empty-lines-after 1)
+      '(
+	
+	("p" "New Project" entry (file+olp "C:\\Users\\josinski\\OneDrive - JNJ\\org\\todos.org" "TODOs")
+         "* PROJECT %^{Enter Project} %^G\n** TODO %^{Enter First Task} %?\n" :empty-lines-after 1)
 
-	  ("q" "Quick Task" entry (file+olp "C:\\Users\\josinski\\OneDrive - JNJ\\org\\todos.org" "TODOs" "Quick Tasks")
+	("t" "Quick Task" entry (file+olp "C:\\Users\\josinski\\OneDrive - JNJ\\org\\todos.org" "TODOs" "Quick Tasks")
          "* TODO %?\n" :empty-lines-after 1)
 
         ("f" "Future Todo" entry (file+olp "C:\\Users\\josinski\\OneDrive - JNJ\\org\\todos.org" "TODOs" "Future Tasks")
@@ -15,12 +16,9 @@
 
         ("m" "Meeting Notes" entry (file+olp "C:\\Users\\josinski\\OneDrive - JNJ\\org\\todos.org" "Meeting Notes")
          "* %t %^{Meeting Title} %^G\n** Attendance\n|Attendee|Present|\n|-|-|\n|%?\n** Notes\n** Action Items\n*** (Begin Todos) " :empty-lines-after 1)
-
-		("w" "Work Notes" entry (file+olp "C:\\Users\\josinski\\OneDrive - JNJ\\org\\todos.org" "Work Notes")
+	
+	("w" "Work Notes" entry (file+olp "C:\\Users\\josinski\\OneDrive - JNJ\\org\\todos.org" "Work Notes")
          "* %? %^G\n" :empty-lines-after 1)
         ))
 
 (provide 'knavemacs-platform)
-
-
-
