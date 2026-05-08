0a79d08172213773c5e6dade9f833b0bf41a9e86
John Osinski <johnosinski80@gmail.com>
2026-05-08 19:19:37 -0400

    custom modeline framework added

---
+++ knavemacs/init-modeline.el
@@ -0,0 +1,43 @@
+;; -*- lexical-binding: t; eval: (local-set-key (kbd "C-c C-c") #'imenu); eval: (setq imenu-generic-expression '(("Sections" "^;;; \\(.*\\)$" 1))); -*-
+
+;; init-modeline.el - custom modeline
+;; Part of Knavemacs
+
+;;
+;;; N1 faces ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(defface knavemacs/modeline-faces--bufname
+  '((t :foreground "#DDDDDD"
+  	   ))
+  "Bufname Face"
+  :group 'knavemacs/modeline-faces)
+
+
+;;
+;;; N2 modules ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+
+;; modeline module: buffer name
+(defvar-local knavemacs/modeline--bufname
+  	'(:eval
+  	  (propertize (buffer-name) 'face 'knavemacs/modeline-faces--bufname))
+  "Modeline module to provide the buffer name.")
+
+;;
+;;; N3 define modules ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(dolist (construct '(
+  					 knavemacs/modeline--bufname))
+  (put construct 'risky-local-variable t)) ;; required for modeline local vars
+
+;;
+;;; N4 modeline construction
+;;
+(setq-default mode-line-format
+  			  '("%e"
+  				;mode-line-front-space
+                                ;mode-line-modes
+  				knavemacs/modeline--bufname))
+;  				(:eval (knavemacs/modeline-fill-for-alignment))
+;  				knavemacs/modeline-kmacro-indicator))
+
