51e7fb87bb77e8b6d491335d7c7b248802a603c8
John Osinski <johnosinski80@gmail.com>
2026-05-16 19:58:39 -0400

    added which-key config

--- knavemacs_base/init-mode.el
+++ knavemacs_base/init-mode.el
@@ -1,31 +1,3 @@
-;; -------------------
-;;; 4.3 which-key-mode
-;; -------------------
-(use-package which-key
-  :defer t
-  :ensure nil
-  :hook
-  (after-init-hook . which-key-mode)
-  :config
-  (setq which-key-separator "  ")
-  (setq which-key-prefix-prefix "... ")
-  (setq which-key-max-display-columns 3)
-  (setq which-key-idle-delay 1.5)
-  (setq which-key-idle-secondary-delay 0.25)
-  (setq which-key-add-column-padding 1)
-  (setq which-key-max-description-length 40)
-
-  (which-key-add-key-based-replacements
-    "SPC o" "Org Commands"
-    "SPC e" "File Explore Commands"
-    "SPC p" "Project Commands"
-    "SPC r" "Register Commands"
-    "SPC g" "Git Commands"
-    "SPC t" "Tab Commands"
-    "SPC h" "Help Commands"
-    "SPC v" "Version Control"
-    "SPC x" "Ctrl-X Commands"))
-
 ;; -----------------
 ;;; 4.4 tab-bar-mode
 ;; -----------------
