1893339f1c847b10ef8366a07b307bc84d51229d
John Osinski <johnosinski80@gmail.com>
2026-05-17 10:50:35 -0400

    added tab bar configuration

--- knavemacs_base/init-mode.el
+++ knavemacs_base/init-mode.el
@@ -1,24 +1,3 @@
-;; -----------------
-;;; 4.4 tab-bar-mode
-;; -----------------
-(use-package tab-bar
-  :ensure nil
-  :defer t
-  :custom
-  (tab-bar-close-button-show nil)
-  (tab-bar-new-button-show nil)
-  (tab-bar-tab-hints t)
-  (tab-bar-auto-width t)
-  (tab-bar-auto-width-min '(10 4))
-  (tab-bar-auto-width-max '(50 5))
-  :init
-  ;; HACK this is an override of the internal function so it
-  ;;      shows only the hint number with some decoration.
-  (defun tab-bar-tab-name-format-hints (name _tab i)
-    "Show absolute numbers on tabs in the tab bar before the tab name.
-  It has effect when `tab-bar-tab-hints' is non-nil."
-    (if tab-bar-tab-hints (concat (format " »%d«" i) "") name)))
-
 ;; ------------------
 ;;; 4.5 tab-line-mode
 ;; ------------------
