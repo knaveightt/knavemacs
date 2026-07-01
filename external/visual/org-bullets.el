3fc603a287eafeff0f2a96c065ea2d4e33c87713
John O <johnosinski80@gmail.com>
2026-07-01 16:03:30 -0400

    update indentation faces when using org-bullets

--- external/visual/org-bullets.el
+++ external/visual/org-bullets.el
@@ -11,6 +11,8 @@
   ;; a fix for color issues with org-indent mode
   (defun knavemacs/org-indent-fix-colors ()
     "Function to adjust some faces when org-bullets are used with org indent mode"
-    (set-face-attribute 'org-hide nil :background "#181a26" :foreground "#181a26")
-    (set-face-attribute 'org-indent nil :background "#181a26" :foreground "#181a26"))
+    (set-face-attribute 'org-hide nil :foreground (face-attribute 'default :background nil t))
+    (set-face-attribute 'org-hide nil :background (face-attribute 'default :background nil t))
+    (set-face-attribute 'org-indent nil :foreground (face-attribute 'default :background nil t))
+    (set-face-attribute 'org-indent nil :background (face-attribute 'default :background nil t)))
   (advice-add 'org-indent-mode :after #'knavemacs/org-indent-fix-colors))
